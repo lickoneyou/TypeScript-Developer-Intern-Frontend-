a8ed0d7bc8ba5fd7dae664f08fb777d5fc2b5201
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-04-09 11:04:59 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/TypeScript-Developer-Intern-Frontend-',
+  base: '/TypeScript-Developer-Intern-Frontend-/dist',
   plugins: [
     vue(),
   ],
